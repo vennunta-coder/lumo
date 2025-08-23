b175a6be4b340b63337850ca43783787be9da0d1
vennunta-coder <vennunta@gmail.com>
2025-08-23 17:49:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// js/main.js
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("header.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("header").innerHTML = data);
+
+  fetch("footer.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("footer").innerHTML = data);
+});
